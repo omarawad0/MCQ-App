c2609015cfa43a3e8d74d584940499e5b3a93f47
omarawad0 <ogmal01@gmail.com>
2021-11-05 21:52:40 +0200

    added Questions component

---
+++ src/components/Questions.js
@@ -0,0 +1,65 @@
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import checkAnswer from "../helpers/checkAnswer";
+import { useSelector, useDispatch } from "react-redux";
+import { nextStep, decrementScore } from "../redux/actions";
+
+function Questions() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answer, setAnswer] = useState(null);
+  const { questions } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { question, answers } = questions[currentQuestion];
+
+  const onAnswerClick = (answerIndex) => {
+    setAnswer(answerIndex);
+  };
+
+  const isLastQuestion = () => {
+    return currentQuestion === questions.length - 1;
+  };
+
+  const handleNextQuestion = () => {
+    if (answer != null) {
+      const isCorrect = checkAnswer(answer);
+      dispatch(decrementScore(!isCorrect));
+      setAnswer(null);
+      isLastQuestion()
+        ? dispatch(nextStep("SCORE"))
+        : setCurrentQuestion((prev) => prev + 1);
+    }
+  };
+
+  return (
+    <Box>
+      <Typography variant="p" fontSize="20px">
+        {question}
+      </Typography>
+      <List sx={{ width: "100%" }} aria-label="contacts" disablePadding>
+        {answers.map((a, i) => (
+          <ListItem key={a} onClick={() => onAnswerClick(i)}>
+            <ListItemButton
+              sx={{ textAlign: "center" }}
+              selected={i === answer}
+            >
+              <ListItemText primary={a} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Button variant="contained" onClick={handleNextQuestion}>
+        {isLastQuestion() ? "Finsih" : "Next"}
+      </Button>
+    </Box>
+  );
+}
+
+export default Questions;
