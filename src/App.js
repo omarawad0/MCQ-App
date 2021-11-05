a8cc306e3bf3870bc2465cd8d7335fcebf3fef9e
omarawad0 <ogmal01@gmail.com>
2021-11-05 21:50:44 +0200

    app render 3 components conditionally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Enroll from "./components/Enroll";
+import Questions from "./components/Questions";
+import Score from "./components/Score";
+import { useSelector } from "react-redux";
 function App() {
+  const { currentStep } = useSelector((state) => state);
+
+  const steps = {
+    ENROLL: <Enroll />,
+    QUESTIONS: <Questions />,
+    SCORE: <Score />,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box textAlign="center" mt={10}>
+        <Typography variant="h2" fontWeight="bold">
+          MCQ App
+        </Typography>
+        {steps[currentStep]}
+      </Box>
+    </Container>
   );
 }
 
