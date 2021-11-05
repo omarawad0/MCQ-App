a5a2351cfa50e03c9ec0e4701c0787d980321ba0
omarawad0 <ogmal01@gmail.com>
2021-11-05 21:51:17 +0200

    added Enroll component to enroll student

---
+++ src/components/Enroll.js
@@ -0,0 +1,49 @@
+import { FormControl, Button, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import shuffleQuestionsHelper from "../helpers/shuffleQuestionHelper";
+import { enrollStudent, nextStep, shuffleQuestions } from "../redux/actions";
+
+function Enroll() {
+  const [studentName, setStudentName] = useState("");
+  const dispatch = useDispatch();
+  const { questions } = useSelector((state) => state);
+
+  const nameChangeHandler = (e) => {
+    const newName = e.target.value;
+    setStudentName(newName);
+  };
+
+  const handleClick = () => {
+    const name = studentName.trim();
+    if (name) {
+      dispatch(enrollStudent(name));
+      const shuffledQuestions = shuffleQuestionsHelper(questions);
+      dispatch(shuffleQuestions(shuffledQuestions));
+      dispatch(nextStep("QUESTIONS"));
+    }
+  };
+
+  return (
+    <Box width="100%" mt={5}>
+      <FormControl fullWidth mb={5}>
+        <TextField
+          variant="outlined"
+          label="Your Name"
+          type="text"
+          size="large"
+          value={studentName}
+          onChange={nameChangeHandler}
+        />
+      </FormControl>
+      <Box width="100%" mt={3}>
+        <Button variant="contained" fullWidth onClick={handleClick}>
+          Next
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default Enroll;
