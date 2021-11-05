cc3725fe547bec572474f6ef31a0d9da5666ba37
omarawad0 <ogmal01@gmail.com>
2021-11-05 21:53:15 +0200

    added Score component to render final score and reset the quiz

---
+++ src/components/Score.js
@@ -0,0 +1,43 @@
+import { Alert, AlertTitle, Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { nextStep, enrollStudent, resetScore } from "../redux/actions";
+
+function Score() {
+  const { score, questions } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const scoreSeverity = () => {
+    const half = Math.floor(questions.length / 2);
+    if (score >= half) {
+      return "success";
+    } else {
+      return "error";
+    }
+  };
+
+  const resetQuiz = () => {
+    dispatch(resetScore(questions.length));
+    dispatch(enrollStudent(""));
+    dispatch(nextStep("ENROLL"));
+  };
+  return (
+    <Box>
+      <Box mt={5}>
+        <Typography variant="h4">Final Score</Typography>
+        <Alert severity={scoreSeverity()}>
+          <AlertTitle>Results</AlertTitle>
+          You scored — <strong>{`${score}/${questions.length}`}</strong>
+        </Alert>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained" onClick={resetQuiz}>
+          Reset
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default Score;
